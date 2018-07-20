28128d1233f15df5c43435a66b0171f32ef700fc
TheRealVaranidae <39166583+TheRealVaranidae@users.noreply.github.com>
2018-07-20 17:32:26 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,3 +5,5 @@ VALIDATE_FILE=$1
 OUTPUT_DIR=$2
 OUTPUT_FILE = ${OUTPUT_DIR}/$(basename $VALIDATE_FILE).txt
 python create_detections.py -c ./multiresfinetuned5296.pb -cs 300 -i $VALIDATE_FILE -o $OUTPUT_DIR
+
+

7d444088e47dce406ec25bddc14b46f708297a4d
TheRealVaranidae <39166583+TheRealVaranidae@users.noreply.github.com>
2018-07-20 15:26:19 +0000

    Update create_detections.sh

--- create_detections.sh
+++ create_detections.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-TESTLIST="X.tif Y.tif"
-
-for i in $TESTLIST; do
-    echo $i
-    python create_detections.py -c ../models/frozen_model.pb -o 'preds_output/'$i'.txt' 'xView/rgb/'$i
-done
+python inference.py -c ./multiresfinetuned5296.pb --input-image $1 --output-dir $2 

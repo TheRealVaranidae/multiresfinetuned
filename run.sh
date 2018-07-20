6a769a929d8f1d096cdcc94569f4b761939e3403
TheRealVaranidae <39166583+TheRealVaranidae@users.noreply.github.com>
2018-07-20 17:47:03 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ set -exuo pipefail
 VALIDATE_FILE=$1
 OUTPUT_DIR=$2
 OUTPUT_FILE = ${OUTPUT_DIR}/$(basename $VALIDATE_FILE).txt
-python create_detections.py -c ./multiresfinetuned5296.pb -cs 300 -i $VALIDATE_FILE -o $OUTPUT_DIR
+python create_detections_1.py -c ./multiresfinetuned5296.pb -cs 300 -i $VALIDATE_FILE -o $OUTPUT_DIR
 
 
